761b18a5896ea8e301d7087a503f2993d5c8a248
kjo7919 <98440232+kjo7919@users.noreply.github.com>
2023-07-18 23:28:11 +0900

    projectSetting (#33)
    * test
    
    test
    
    * 로그인페이지, 회원가입페이지(본인인증)
    
    * 로그인 페이지 경로 수정
    
    * 회원가입 페이지 js기능 추가
    
    * 랭킹 페이지 작업
    
    회원가입 페이지 js추가
    
    * projectSetting
    
    projectSetting

---
+++ LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/LunchBoxDonationApplication.java
@@ -0,0 +1,13 @@
+package com.lunchbox.lunchboxdonation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchBoxDonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchBoxDonationApplication.class, args);
+	}
+
+}
---
+++ LunchBoxDonation/src/test/java/com/lunchbox/lunchboxdonation/LunchBoxDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunchbox.lunchboxdonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchBoxDonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
