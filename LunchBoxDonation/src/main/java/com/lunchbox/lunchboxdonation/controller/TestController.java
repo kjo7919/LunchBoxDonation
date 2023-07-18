0c42b8893684a3179dcc9cd500eaa5d0416c41bb
kjo7919 <98440232+kjo7919@users.noreply.github.com>
2023-07-19 00:32:57 +0900

    포르잭트 세팅 (#34)
    * test
    
    test
    
    * 로그인페이지, 회원가입페이지(본인인증)
    
    * 로그인 페이지 경로 수정
    
    * 회원가입 페이지 js기능 추가
    
    * 랭킹 페이지 작업
    
    회원가입 페이지 js추가
    
    * projectSetting
    
    projectSetting
    
    * 경로및 파일 세팅
    
    css, js, 이미지 경로는 각자 수정부탁드려요

---
+++ LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.lunchbox.lunchboxdonation.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Slf4j
+public class TestController {
+
+    @RequestMapping("")
+    public ModelAndView test(){
+        ModelAndView mv = new ModelAndView();
+
+        log.info("test");
+        mv.setViewName("test");
+        return mv;
+    }
+}
