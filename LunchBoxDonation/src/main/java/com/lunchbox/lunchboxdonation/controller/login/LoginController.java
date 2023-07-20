3bf70195f0e0231162da4c3b4d02e31f5dfaf415
kjo7919 <98440232+kjo7919@users.noreply.github.com>
2023-07-20 16:09:07 +0900

    아이디, 비밀번호 찾기 페이지 추가 css 수정 controller 설정

---
+++ LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/controller/login/LoginController.java
@@ -0,0 +1,76 @@
+package com.lunchbox.lunchboxdonation.controller.login;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Slf4j
+@RequestMapping("login")
+public class LoginController {
+
+    @RequestMapping("login")
+    public ModelAndView login(){
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("login/login"); // html 파일 경로
+
+        return mv;
+    }
+
+
+    @RequestMapping("foundId")
+    public ModelAndView foundId(){
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("login/foundId"); // html 파일 경로
+
+        return mv;
+    }
+
+    @RequestMapping("foundPw")
+    public ModelAndView foundPw(){
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("login/foundPw"); // html 파일 경로
+
+        return mv;
+    }
+
+    @RequestMapping("join")
+    public ModelAndView join(){
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("login/joinPage"); // html 파일 경로
+
+        return mv;
+    }
+
+    @RequestMapping("reset")
+    public ModelAndView resetPw(){
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("login/resetPw"); // html 파일 경로
+
+        return mv;
+    }
+
+    @RequestMapping("successFound")
+    public ModelAndView successFoundId(){
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("login/successFoundId"); // html 파일 경로
+
+        return mv;
+    }
+
+    @RequestMapping("ranking")
+    public ModelAndView ranking(){
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("login/rankingPage"); // html 파일 경로
+
+        return mv;
+    }
+}
