3818475710a186b19332023970519ad0a6e7ffa4
PyoChangYeong <107843779+PyoChangYeong@users.noreply.github.com>
2023-07-23 09:16:25 +0900

    Controller (#45)
    * #1 fix: ChangYeong Test
    
    * feat: mypage 07.23 (css 이미지 미완성)
    
    * feat: mypage Controller

---
+++ LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/controller/mypageController.java
@@ -0,0 +1,20 @@
+package com.lunchbox.lunchboxdonation.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Slf4j
+public class mypageController {
+
+    @RequestMapping("mypage")
+    public ModelAndView mypage(){
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("mypage/mypage"); // html 파일 경로
+        log.info(String.valueOf(mv));
+        return mv;
+    }
+}
