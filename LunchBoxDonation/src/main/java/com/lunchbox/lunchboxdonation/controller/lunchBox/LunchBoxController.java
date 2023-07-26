60d1027643bd1dc008faa6a6c66c2bd0ad361cf2
JAEWEONLEE <34277606+jaeweon@users.noreply.github.com>
2023-07-26 22:48:47 +0900

    controller 추가 / 경로 수정 (#54)
    * feat: 2
    
    feat: 2
    
    * FEAT : HEADER/FOOTER
    
    HEADER/FOOTER
    
    * feat : mainPage.html / css
    
    feat : mainPage.html / css
    
    * 메인페이지 수정
    
    메인페이지 수정
    
    * 메인페이지 assets 추가
    
    메인페이지 assets 추가
    
    * 메인페이지 경로 수정
    
    메인페이지 경로 수정
    
    * mainpage, header-footer, dealMonthpage
    
     header-footer 90% 수정
    dealOfMonth page 추가
    
    * Update README.md
    
    * benefitPage
    
    benefitPage
    
    * 도시락 상세보기 페이지
    
    header / footer 수정
    도시락 상세보기 페이지
    
    * 프론트 페이지 수정
    
    이벤트 페이지
    이달의 특가 페이지
    header 수정
    
    * 페이지 상대경로 수정
    
    메인페이지 이미지 상대경로 수정
    
    * change branch
    
    change branch
    update mainPage
    
    * header 간결화
    
    header 간결화
    
    * 이미지 url 변경
    
    이미지 url 변경
    
    * Update read.html
    
    결제 페이지 수정
    
    * 인텔리제이로 작업파일 이전 및 경로 수정
    
    인텔리제이로 작업파일 이전 및 경로 수정
    
    * Revert "인텔리제이로 작업파일 이전 및 경로 수정"
    
    This reverts commit d9e8b2f537c7139d6627a832835c133a9339a8ff.
    
    * 디렉토리 정리
    
    디렉토리 정리
    
    * Revert "디렉토리 정리"
    
    This reverts commit 613c3d46fa4630c9b444ff65bd124631ddc61cc9.
    
    * 컨트롤러 및 경로 수정
    
    컨트롤러 및 경로 수정
    
    * 동일 컨트롤러명 수정
    
    동일 컨트롤러명 때문에 오류 해결
    
    * controller 추가 / 경로 수정
    
    category controller 추가
    경로 수정

--- LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/controller/lunchBox/LunchBoxController.java
+++ LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/controller/lunchBox/LunchBoxController.java
@@ -2,8 +2,16 @@ package com.lunchbox.lunchboxdonation.controller.lunchBox;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequestMapping("mainPage")
 public class LunchBoxController {
+
+    @GetMapping("read")
+    public void read(){
+
+    }
 }
