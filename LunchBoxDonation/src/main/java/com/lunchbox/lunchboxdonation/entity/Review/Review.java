d846aeea9ac6f4fe8e1ddb5d6517ce2a33e3d85c
JAEWEONLEE <34277606+jaeweon@users.noreply.github.com>
2023-07-30 00:24:37 +0900

    feat : review Entity update (#65)
    * feat: 2
    
    feat: 2
    
    * FEAT : HEADER/FOOTER
    
    HEADER/FOOTER
    
    * feat : mainPage.html / css
    
    feat : mainPage.html / css
    
    * 메인페이지 수정
    
    메인페이지 수정
    
    * 메인페이지 assets 추가
    
    메인페이지 assets 추가
    
    * 메인페이지 경로 수정
    
    메인페이지 경로 수정
    
    * mainpage, header-footer, dealMonthpage
    
     header-footer 90% 수정
    dealOfMonth page 추가
    
    * Update README.md
    
    * benefitPage
    
    benefitPage
    
    * 도시락 상세보기 페이지
    
    header / footer 수정
    도시락 상세보기 페이지
    
    * 프론트 페이지 수정
    
    이벤트 페이지
    이달의 특가 페이지
    header 수정
    
    * 페이지 상대경로 수정
    
    메인페이지 이미지 상대경로 수정
    
    * change branch
    
    change branch
    update mainPage
    
    * header 간결화
    
    header 간결화
    
    * 이미지 url 변경
    
    이미지 url 변경
    
    * Update read.html
    
    결제 페이지 수정
    
    * 인텔리제이로 작업파일 이전 및 경로 수정
    
    인텔리제이로 작업파일 이전 및 경로 수정
    
    * Revert "인텔리제이로 작업파일 이전 및 경로 수정"
    
    This reverts commit d9e8b2f537c7139d6627a832835c133a9339a8ff.
    
    * 디렉토리 정리
    
    디렉토리 정리
    
    * Revert "디렉토리 정리"
    
    This reverts commit 613c3d46fa4630c9b444ff65bd124631ddc61cc9.
    
    * 컨트롤러 및 경로 수정
    
    컨트롤러 및 경로 수정
    
    * 동일 컨트롤러명 수정
    
    동일 컨트롤러명 때문에 오류 해결
    
    * controller 추가 / 경로 수정
    
    category controller 추가
    경로 수정
    
    * 관리자 컨트롤러 추가
    
    관리자 컨트롤러 추가
    
    * 어드민 CSS 경로 수정
    
    어드민 CSS 경로 수정
    
    * feat : review Entity
    
    feat : review Entity
    
    * feat : review Entity update
    
    feat : review Entity update

--- LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/entity/Review/Review.java
+++ LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/entity/Review/Review.java
@@ -1,17 +1,14 @@
 package com.lunchbox.lunchboxdonation.entity.Review;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import com.lunchbox.lunchboxdonation.entity.Member;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter @ToString
 @NoArgsConstructor
+@Table(name = "TBL_REVIEW")
 public class Review {
     @Id
     @GeneratedValue
@@ -19,6 +16,32 @@ public class Review {
 
     private String reviewContent;
 
+    //    멤버 연관 관계
+    // 다대일 관계: 여러 개의 리뷰는 하나의 멤버와 연관될 수 있음
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id") // 이 부분은 외래키(Foreign Key) 컬럼의 이름을 지정.
+    private Member member;
+
+    @Builder
+    public Review(Long id, String reviewContent) {
+
+        this.id = id;
+        this.reviewContent = reviewContent;
+    }
+
+
 //    도시락 연관 관계
-//    멤버 연관 관계
+
+//    @Builder는 Lombok 어노테이션 중 하나로, 주로 빌더 패턴을 자동으로 생성해주는 역할을 합니다.
+//    빌더 패턴은 객체를 생성할 때 사용하는 디자인 패턴 중 하나로, 객체 생성에 사용되는 매개변수가 많을 때 코드의 가독성과 유지보수성을 향상시키기 위해 사용됩니다.
+//    @Builder 어노테이션을 클래스에 추가하면, 해당 클래스에 대해 빌더 패턴을 사용할 수 있는 빌더 클래스를 자동으로 생성해줍니다.
+
+//    이렇게 생성된 빌더 클래스는 원하는 필드만을 선택적으로 설정할 수 있도록 만들어주며, 빌더 메서드를 통해 인스턴스를 생성할 수 있게 됩니다.
+//    @Builder 어노테이션의 역할은 다음과 같습니다:
+
+//    빌더 패턴 자동 생성: @Builder를 사용하면 Lombok이 자동으로 빌더 패턴을 생성해주기 때문에 개발자가 직접 빌더 패턴을 구현할 필요가 없습니다.
+//    선택적 필드 설정: 빌더 클래스를 통해 인스턴스를 생성할 때, 필요한 필드만 선택적으로 설정할 수 있어서 코드를 간결하고 가독성 있게 만들어줍니다.
+
+//    불변성 보장: 빌더 패턴을 사용하면 생성된 인스턴스가 불변성을 가질 수 있도록 도와줍니다. 즉, 생성된 후에는 값을 변경할 수 없는 불변 객체로 만들 수 있습니다.
+//    @Builder 어노테이션을 사용하면 생성자에 비해 훨씬 명시적이고 간결한 방법으로 객체를 생성할 수 있으며, 특히 많은 매개변수를 가지는 클래스의 경우 더욱 유용하게 사용됩니다.
 }
